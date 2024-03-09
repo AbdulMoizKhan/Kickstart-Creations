e9fcd30a768795812fb5c39fa31f121b5d7e8a51
Abdul Moiz Khan <73781652+AbdulMoizKhan@users.noreply.github.com>
2024-03-09 20:31:28 +0500

    first commit mui+vite

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+     <Button variant="text">Text</Button>
+      Hello, Material-UI!
+    </>
+  )
+}
+
+export default App
