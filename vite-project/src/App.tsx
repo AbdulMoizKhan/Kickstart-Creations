120c43db486889e72e9f795d6b047b99f60e1062
Abdul Moiz Khan <73781652+AbdulMoizKhan@users.noreply.github.com>
2024-03-26 05:09:58 +0500

    deployment issues resolved

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router';
 import { ThemeProvider } from '@mui/material';
-import { theme } from '../theme.ts'; // Assuming you have a theme defined
+import { theme } from './theme.ts'; // Assuming you have a theme defined
 
 import LandingPageLayout from './layout';
 import { Home } from './routes/home';
